01ffed895972202b13861a138b7a37a8ef9d1e5b
Nms <80737367+Madhav-nms@users.noreply.github.com>
2026-08-03 18:26:18 -0400

    Time: 3 ms (6.27%) | Memory: 27.3 MB (16.05%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -3,12 +3,12 @@ public:
     void reverseString(vector<char>& s) {
     int left = 0;
     int right = s.size() - 1;
-    while (left < right) {
-    char temporary = s[left];
-    s[left] = s[right];
-    s[right] = temporary;
-    left++;
-    right--;
-    }
-    }
+    while(left < right){
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }  
+};
 };
