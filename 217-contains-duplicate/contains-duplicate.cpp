c588ee19da58829d04fe2607e60468d76f64c1b7
Nms <80737367+Madhav-nms@users.noreply.github.com>
2026-08-29 02:05:25 -0400

    Time: 75 ms (44.29%) | Memory: 111.2 MB (73.98%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set <int> u_set;
+        for(int i = 0; i < nums.size(); i++){
+        if(u_set.find(nums[i]) != u_set.end()){
+            return true;
+        }
+        else {
+            u_set.insert(nums[i]);
+        }
+    }
+    return false;
+    }
+};
