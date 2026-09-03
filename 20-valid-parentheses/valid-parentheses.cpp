553b392f576f29bd74c448b628939c54d78306ae
Nms <80737367+Madhav-nms@users.noreply.github.com>
2026-09-03 14:02:15 -0400

    Time: 0 ms (100.00%) | Memory: 8.8 MB (88.95%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for (char ch : s){
-            if (ch == '(' || ch == '{' || ch == '[') {
-            st.push(ch);
-        }
-            else {
-            if (st.empty()) return false;
-            
-            if ((ch == ')' && st.top() == '(') || 
-               (ch == '}' && st.top() == '{') || 
-               (ch == ']' && st.top() == '[')) {
-                st.pop();
-               }
-            else {
-                return false;
+        stack <char> st;
+        if (s.empty()) return false;
+        for (char val : s) {
+            if (val == '[' || val == '{' || val == '('){
+                st.push(val);
             }
-        }
+            else {
+                if (st.empty()) return false;
+                else if (val == '}' && st.top() == '{') st.pop();
+                else if (val == ']' && st.top() == '[') st.pop();
+                else if (val == ')' && st.top() == '(') st.pop();
+                else return false;
+            }
         }
         return st.empty();
     }
