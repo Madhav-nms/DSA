47a8876bf51410574b4b4df5396f86175fe82159
Nms <80737367+Madhav-nms@users.noreply.github.com>
2026-09-06 12:57:09 -0400

    Time: 0 ms (100.00%) | Memory: 10.7 MB (57.12%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -3,13 +3,7 @@ public:
     vector<int> countBits(int n) {
         vector <int> ans(n+1, 0);
         for (int i = 0; i < n+1; i++){
-            int x = i; 
-            int count = 0; 
-            while (x > 0) {
-                count = count + (x & 1);
-                x = x >> 1;
-            }
-            ans[i] = count;
+            ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
     }
